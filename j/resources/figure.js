6d83e7c01a903d7a0746dea73a16d8935d4504da
Joe Marchell <nullfutures@gmail.com>
2018-10-24 23:40:27 -0400

    uploading updated code

---
+++ j/resources/figure.js
@@ -0,0 +1,44 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": true, 
+        "title": "Actual Toxicity Percentages", 
+        "hiddenlabels": [], 
+        "xaxis": {
+            "range": [
+                -1, 
+                6
+            ], 
+            "autorange": true
+        }, 
+        "yaxis": {
+            "range": [
+                -1, 
+                4
+            ], 
+            "autorange": true
+        }
+    }, 
+    "data": [
+        {
+            "labelssrc": "nullfutures:32:0dbcda", 
+            "labels": [
+                "", 
+                "nontoxic", 
+                "toxic"
+            ], 
+            "values": [
+                "Actual", 
+                "36069", 
+                "3824"
+            ], 
+            "mode": "markers", 
+            "marker": {
+                "colors": []
+            }, 
+            "textinfo": "label+percent", 
+            "type": "pie", 
+            "valuessrc": "nullfutures:32:5ce34b"
+        }
+    ]
+}
